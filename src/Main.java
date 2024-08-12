95bf680a532e9f5a4b33277800955a98b3fb3cf5
Anusha853 <mahendraanusha153@gmail.com>
2024-08-12 15:45:54 +0530

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main{
 	public static void main(String[] args){
 		System.out.println("===== Welcome to Java project =====");
+		Customer c1=new Customer(111, "Mohan", "Bangalore", "mohan@gmail.com");
+		System.out.println(c1);
 	}
 }
